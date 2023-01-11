341bc9837662a705e06a4b163dffb01215e7c523
ijaz hanif <ijazhanif767@gmail.com>
2023-01-11 21:54:31 +0000

    first init

---
+++ Scend.js
@@ -0,0 +1,55 @@
+  // Question No#1
+
+let MyData=new Object();
+
+MyData.f_name='ijaz';
+MyData.L_name='Hanif';
+MyData.height=5.6;
+
+MyData.talk=function(){
+    console.log('when alive');
+}
+console.log(MyData);
+
+              // Question No#2
+      
+              
+              let car={
+                name:'Ferrari',
+                color:'blue',
+                model:2019
+            };
+                  car.name='SJeep Gladiator';
+                  car.color='white';
+                  car.model=2020;
+
+              console.log(car.color);
+              console.log(car.model);
+              
+              // Question No#3
+             
+              function name(){
+                  console.log(' Ijaz ')
+                }
+                name();
+              
+                // Question No#3
+             function Add(a,b){
+               console.log('sum of a and b is: ',(a+b));
+             }
+             Add(5,15);
+
+             function Subtract(a,b){
+               console.log('sum of a and b is: '+ (a-b));
+             }
+             Subtract(5,15);
+
+             function Multiplication(a,b){
+               console.log('sum of a and b is: '+ (a*b));
+             }
+             Multiplication(5,15);
+
+             function Division(a,b){
+               console.log('sum of a and b is: '+ (a/b));
+             }
+             Division(5,15);
